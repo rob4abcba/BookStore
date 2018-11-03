7495e491b8d719189e83ad643eefc69c9a270777
Lisa Gray <lisa.gray@youversion.com>
2018-11-02 20:20:34 -0600

    finished code in all files, runtime error

---
+++ app/src/main/java/com/example/android/bookstore/BooksCursorAdapter.java
@@ -0,0 +1,79 @@
+package com.example.android.bookstore.data;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.android.bookstore.R;
+import com.example.android.bookstore.data.BookContract;
+
+/**
+ * {@link BooksCursorAdapter} is an adapter for a list or grid view
+ * that uses a {@link Cursor} of book data as its data source. This adapter knows
+ * how to create list items for each row of book data in the {@link Cursor}.
+ */
+public class BooksCursorAdapter extends CursorAdapter {
+
+    /**
+     * Constructs a new {@link BooksCursorAdapter}.
+     *
+     * @param context The context
+     * @param c       The cursor from which to get the data.
+     */
+    public BooksCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0 /* flags */);
+    }
+
+    /**
+     * Makes a new blank list item view. No data is set (or bound) to the views yet.
+     *
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already
+     *                moved to the correct position.
+     * @param parent  The parent to which the new view is attached to
+     * @return the newly created list item view.
+     */
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+    }
+
+    /**
+     * This method binds the book data (in the current row pointed to by cursor) to the given
+     * list item layout. For example, the name for the current book can be set on the name TextView
+     * in the list item layout.
+     *
+     * @param view    Existing view, returned earlier by newView() method
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already moved to the
+     *                correct row.
+     */
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        //Find individual views to modify in the list item layout
+        TextView nameTextView = (TextView) view.findViewById(R.id.name);
+        TextView priceTextView = (TextView) view.findViewById(R.id.price);
+        TextView quantityTextView = (TextView) view.findViewById(R.id.quantity);
+
+        //Find columns of book attributes that we're interested in
+        int nameColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_BOOK_NAME);
+        int priceColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_BOOK_PRICE);
+        int quantityColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_BOOK_QUANTITY);
+
+        //Read the book attributes from the cursor for the current book
+        String bookName = cursor.getString(nameColumnIndex);
+        int bookPrice = cursor.getInt(priceColumnIndex);
+        int bookQuantity = cursor.getInt(quantityColumnIndex);
+
+        //Update the TextViews with the attributes of the current book
+        nameTextView.setText(bookName);
+        priceTextView.setText(bookPrice);
+        quantityTextView.setText(bookQuantity);
+    }
+}
+
